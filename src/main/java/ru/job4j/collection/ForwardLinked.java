f12076754d6f4ef6174f3d84a7f92c22e46da173
javaDevArtem <artemjdev@gmail.com>
2023-09-16 20:44:07 -0400

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,99 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> curr = this.head;
+        if (this.head == null) {
+            this.head = new Node<>(value, null);
+            size++;
+            modCount++;
+            return;
+        }
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = new Node<>(value, null);
+        size++;
+        modCount++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> curr = head;
+        int i = 0;
+        while (index != i) {
+            curr = curr.next;
+            i++;
+        }
+        return curr.item;
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        Node<T> curr = head;
+        Node<T> next = null;
+        if (curr.next != null) {
+            next = curr.next;
+        }
+        T deletedItem = curr.item;
+        curr.item = null;
+        curr.next = null;
+        head = next;
+        size--;
+        modCount--;
+        return deletedItem;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int cursor = 0;
+            final int expModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (expModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<T> curr = head;
+                for (int i = 0; i < cursor; i++) {
+                    curr = curr.next;
+                }
+                cursor++;
+                return curr.item;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
