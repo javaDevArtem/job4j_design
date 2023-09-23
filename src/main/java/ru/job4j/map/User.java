3e8838fa0472818677244dde32b31f7c1d60ca9e
javaDevArtem <artemjdev@gmail.com>
2023-09-23 18:35:42 -0400

    4. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
 
@@ -12,5 +14,35 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Stas", 3, Calendar.getInstance());
+        int hashCode1 = user1.hashCode();
+        int hash1 = hashCode1 ^ (hashCode1 >>> 16);
+        int bucket1 = hash1 & 15;
+        User user2 = new User("Stas", 3, Calendar.getInstance());
+        int hashCode2 = user2.hashCode();
+        int hash2 = hashCode2 ^ (hashCode2 >>> 16);
+        int bucket2 = hash2 & 15;
+
+        System.out.printf("User1 hashCode1: %s, хэш: %s, бакет: %s \n",
+                hashCode1, hash1, bucket1);
+        System.out.printf("User2 hashCode2: %s, хэш: %s, бакет: %s \n",
+                hashCode2, hash2, bucket2);
+        System.out.println("---");
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                ", birthday=" + birthday.getTime() +
+                '}';
     }
 }
